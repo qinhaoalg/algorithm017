7dd8e781fb27240fd6c2b4e89cb5a4560ea3410c
qinhaoalg <71977935+qinhaoalg@users.noreply.github.com>
2020-10-18 20:49:52 +0800

    Create 236.lowest-common-ancestor-of-a-binary-tree.cpp

---
+++ Week_03/236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        return left ? left : right;
+    }
+};
+
+// 作者：guohaoding
+// 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-jian-j/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
