a579ab965cc59c7367ab15fc51533e27409dd1bc
qinhaoalg <71977935+qinhaoalg@users.noreply.github.com>
2020-11-09 00:58:52 +0800

    45.jump-game-ii.txt (#10)
    * Create 45.jump-game-ii.txt
    
    * Create Solution.java
    
    * Create SolutionTest.java

---
+++ Week_06/solution45/Solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0; 
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]); 
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
